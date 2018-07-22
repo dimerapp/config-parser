b229cb04658f8fcb492e96c132fde94a628d417e
thetutlage <virk.officials@gmail.com>
2018-07-22 21:18:32 +0530

    feat: initial working commit

---
+++ README.md
@@ -0,0 +1,93 @@
+<div align="center">
+  <div>
+    <img width="500" src="https://res.cloudinary.com/adonisjs/image/upload/q_100/v1532274184/Dimer_Readme_Banner_lyy7wv.svg" alt="Dimer App">
+  </div>
+  <br>
+  <p>
+    <a href="https://dimerapp.com/what-is-dimer">
+      Dimer is an open source project and CMS to help you publish your documentation online.
+    </a>
+  </p>
+  <br>
+  <p>
+    <sub>We believe every project/product is incomplete without documentation. <br /> We want to help you publish user facing documentation, without worrying <code>about tools or code</code> to write.</sub>
+  </p>
+  <br>
+</div>
+
+# Dimer Config Parser
+> Parses dimer.json
+
+[![travis-image]][travis-url]
+[![npm-image]][npm-url]
+
+This module parses the `dimer.json` file and returns a normalised config object back.
+
+## Installation
+
+```shell
+npm i @dimerapp/config-parser
+
+# yarn
+yarn add @dimerapp/config-parser
+```
+
+## Usage
+
+```js
+const ConfigParser = require('@dimerapp/config-parser')
+
+const configParser = new ConfigParser()
+
+// optionally with custom config file
+const config = new ConfigParser(join(__dirname, 'dimer.json'))
+```
+
+## API
+
+#### parse
+Parse the config file and returns normalised config object.
+
+```js
+const { errors, config } = await configParser.parse()
+
+if (errors) {
+  errors.forEach(console.log)
+  return
+}
+
+// use config
+```
+
+#### init
+Create a new config file (if missing).
+
+```js
+await configParser.init()
+
+// or pass custom config
+
+await configParser.init({
+  domain: '',
+  versions: {}
+})
+```
+
+## Change log
+
+The change log can be found in the [CHANGELOG.md](https://github.com/dimerapp/config-parser/CHANGELOG.md) file.
+
+## Contributing
+
+Everyone is welcome to contribute. Please take a moment to review the [contributing guidelines](CONTRIBUTING.md).
+
+## Authors & License
+[thetutlage](https://github.com/thetutlage) and [contributors](https://github.com/dimerapp/config-parser/graphs/contributors).
+
+MIT License, see the included [MIT](LICENSE.md) file.
+
+[travis-image]: https://img.shields.io/travis/dimerapp/config-parser/master.svg?style=flat-square&logo=travis
+[travis-url]: https://travis-ci.org/dimerapp/config-parser "travis"
+
+[npm-image]: https://img.shields.io/npm/v/@dimerapp/config-parser.svg?style=flat-square&logo=npm
+[npm-url]: https://npmjs.org/package/@dimerapp/config-parser "npm"
---
+++ index.js
@@ -0,0 +1,189 @@
+/*
+* config-parser
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+const { join } = require('path')
+const fs = require('fs-extra')
+const normalizeUrl = require('normalize-url')
+const url = require('url')
+const _ = require('lodash')
+
+/**
+ * Parse dimer.json file and returns a normalized object
+ *
+ * @class ConfigParser
+ *
+ * @param {String} configPath
+ */
+class ConfigParser {
+  constructor (configPath) {
+    this.configPath = configPath || join(process.cwd(), 'dimer.json')
+    this.defaults = {
+      domain: '',
+      cname: '',
+      defaultVersion: 'master',
+      versions: {
+        master: 'docs/master'
+      },
+      options: {}
+    }
+  }
+
+  /**
+   * Normalizes the domain
+   *
+   * @method _normalizeDomain
+   *
+   * @param  {String}         domain
+   *
+   * @return {String}
+   *
+   * @private
+   */
+  _normalizeDomain (domain) {
+    return domain ? domain.replace(/^\/|[/.]$/g, '') : ''
+  }
+
+  /**
+   * Normalizes cname
+   *
+   * @method _normalizeCname
+   *
+   * @param  {String}        cname
+   *
+   * @return {String}
+   */
+  _normalizeCname (cname) {
+    return cname ? url.parse(normalizeUrl(cname)).hostname : ''
+  }
+
+  /**
+   * Normalizes versions
+   *
+   * @method _normalizeVersions
+   *
+   * @param  {Object}           versions
+   * @param  {String}           defaultVersion
+   *
+   * @return {Array}
+   *
+   * @private
+   */
+  _normalizeVersions (versions, defaultVersion) {
+    return _.map(versions, (version, no) => {
+      version = typeof (version) === 'string' ? { location: version, no } : _.assign({}, version, { no })
+      version.default = !!(defaultVersion && version.no === defaultVersion)
+
+      return version
+    })
+  }
+
+  /**
+   * Validates the domain name
+   *
+   * @method _validateDomain
+   *
+   * @param  {String}        domain
+   * @param  {Array}         errorsBag
+   *
+   * @return {void}
+   *
+   * @private
+   */
+  _validateDomain (domain, errorsBag) {
+    if (!domain) {
+      errorsBag.push('{domain} property missing in dimer.json')
+    }
+  }
+
+  /**
+   * Validates versions
+   *
+   * @method _validateVersions
+   *
+   * @param  {String}          versions
+   * @param  {Array}           errorsBag
+   *
+   * @return {void}
+   */
+  _validateVersions (versions, errorsBag) {
+    _.each(versions, (version) => {
+      if (!version.location) {
+        errorsBag.push(`Make sure to define {docs directory} for version ${version.no}`)
+      }
+    })
+  }
+
+  /**
+   * Parses the config contents
+   *
+   * @method _parseConfigContents
+   *
+   * @param  {Object}             config
+   *
+   * @return {Object}
+   *
+   * @private
+   */
+  _parseConfigContents (config = {}) {
+    const errors = []
+
+    const domain = this._normalizeDomain(config.domain)
+    const cname = this._normalizeCname(config.cname)
+    const versions = this._normalizeVersions(config.versions || {}, config.defaultVersion)
+    const options = config.options || {}
+
+    this._validateDomain(domain, errors)
+    this._validateVersions(versions, errors)
+
+    return { errors, config: { domain, cname, versions, options } }
+  }
+
+  /**
+   * Parse the config file by reading it from the disk
+   *
+   * @method parse
+   *
+   * @return {Object}
+   */
+  async parse () {
+    try {
+      const config = await fs.readJSON(this.configPath, 'utf-8')
+      return this._parseConfigContents(config)
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        return { errors: ['Cannot find dimer.json file'], config: {} }
+      }
+
+      throw error
+    }
+  }
+
+  /**
+   * Creates the config file (only if not already exists)
+   *
+   * @method init
+   *
+   * @param  {Object} options
+   *
+   * @return {Boolean}
+   */
+  async init (options) {
+    const exists = await fs.exists(this.configPath)
+    if (exists) {
+      return false
+    }
+
+    const config = _.merge({}, this.defaults, options)
+    await fs.outputJSON(this.configPath, config)
+
+    return true
+  }
+}
+
+module.exports = ConfigParser
